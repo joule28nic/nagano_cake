0445d717cf9befea4431a52215c2f25e66cd7e5c
joule28nic <153733673+joule28nic@users.noreply.github.com>
2024-02-20 14:42:25 +0000

    24/2/20

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  
+
+  get 'admin/items' => 'admin/items#new'
+
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
