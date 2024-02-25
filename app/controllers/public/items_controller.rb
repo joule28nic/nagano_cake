afe3a49c4125116bafd05f5f36cc6e5eb7a28225
joule28nic <153733673+joule28nic@users.noreply.github.com>
2024-02-25 12:39:15 +0000

    admin/items/finish

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,18 @@
 class Public::ItemsController < ApplicationController
+
+  def index
+    @items = Item.page(params[:page])
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :introduction, :price, :genre_id, :is_active)
+  end
+
 end
